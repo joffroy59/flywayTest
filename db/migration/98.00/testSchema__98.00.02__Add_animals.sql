f52bf5d50fd18e2aaa66be118cc103c8ff6317f0
Jerome OFFROY <jerome.offroy@gmail.com>
2015-09-22 19:56:38 +0200

    add another version folder with content - baseline was between the 2 version folder   - if mvn flyway:baseline NOT execute the content was executed OK   - if mvn flyway:baseline          execute the content was executed KO

---
+++ db/migration/98.00/testSchema__98.00.02__Add_animals.sql
@@ -0,0 +1,4 @@
+insert into ANIMAL (ID, RACE) values (1, 'Lion');
+insert into ANIMAL (ID, RACE) values (2, 'Tiger');
+insert into ANIMAL (ID, RACE) values (3, 'Eagle');
+insert into ANIMAL (ID, RACE) values (4, 'Elephant');
