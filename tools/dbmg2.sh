3fc30f6a73ff5f4d6895fde86690bb32fac9b2f1
Jerome OFFROY <jerome.offroy@gmail.com>
2015-11-21 21:56:31 +0100

    add function traceDebug

--- tools/dbmg2.sh
+++ tools/dbmg2.sh
@@ -34,6 +34,12 @@ function trace {
 	fi
 }
 
+function traceDebug {
+	if $debug ; then
+		traceCmd "[DEBUG] $@"
+	fi
+}
+
 function traceGit {
 	trace "$GIT_PREFIX $@"
 }
@@ -75,10 +81,10 @@ function parseArgsOptions {
 	while getopts ":wxyzvsdhg:t:" opt; do
 	  case $opt in
 		t)
-		  echo "version : $OPTARG"
+		  traceDebug "version : $OPTARG"
 		  ;;
 		g)
-		  echo "git was triggered! Parameter: $OPTARG"
+		  traceDebug "git was triggered! Parameter: $OPTARG"
 		  ;;
 		v)
 		  traceCmdVerbose "-v was triggered!"
@@ -261,6 +267,9 @@ silent=false
 # args parsing 
 #######################
 
+
+traceDebug "@=$@"
 parseArgs "$@"
+traceDebug "@=$@"
 
 exit 5
